64cf1d4a3ff24a4015a9124b92fa42b6dd867a61
Leonid Gavrilov <113637497+lxstcxntxry@users.noreply.github.com>
2023-01-05 19:26:27 +0300

    Delete lab3 directory

--- lab3/src/help/PuppetInterface.java
+++
@@ -1,5 +0,0 @@
-package help;
-
-public interface PuppetInterface {
-    String getName();
-}
--- lab3/src/help/ThingsInterface.java
+++
@@ -1,9 +0,0 @@
-package help;
-
-public interface ThingsInterface {
-    boolean getWindow();
-
-    boolean getPuppet();
-
-    String getName();
-}
